5e9cb1f75764e11fb473f2f456e05d12ad293386
Krunal <kpatel7@wm.com>
2025-08-14 08:15:29 -0500

    Use Streamlit secrets to retrieve GROQ API key

--- streamlitChatBot.py
+++ streamlitChatBot.py
@@ -4,7 +4,7 @@ from dotenv import find_dotenv, load_dotenv
 from langchain_groq import ChatGroq
 
 load_dotenv(find_dotenv())
-api__key = str(os.getenv("GROQ_API_KEY"))
+api__key = st.secrets["GROQ_API_KEY"]
 
 model = ChatGroq(
     model="meta-llama/llama-4-scout-17b-16e-instruct",
